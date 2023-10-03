65c30fc14f557f878d4ee5b8bd2d0e3e4995cb74
Kanyerezi30 <kanyerezi30@gmail.com>
2023-10-03 17:33:07 +0300

    installation

--- setup.sh
+++ setup.sh
@@ -10,18 +10,16 @@ cd ~
 git clone http://github.com/PoonLab/sierra-local
 cd sierra-local
 sudo "${pref_conda}"/envs/HIV-DRIVES/bin/python3 setup.py install
+ mv ${HOME}/HIV-DRIVES/algorithm "${HIVDIR}"/accessories/
 "${pref_conda}"/envs/HIV-DRIVES/bin/python3 "${HIVDIR}"/algorithm/updater.py
-cd HIV-DRIVES
+cd ${HOME}/HIV-DRIVES
 conda create --prefix ${pref_conda}/envs/HIV-DRIVES/quasi -c bioconda quasitools=0.7.0 -y
 wget https://genome-idx.s3.amazonaws.com/bt/GRCh38_noalt_as.zip
 mkdir -p data/reference
 unzip GRCh38_noalt_as.zip
 mv GRCh38_noalt_as data/reference
 rm GRCh38_noalt_as.zip
-mkdir -p "${pref_conda}"/envs/HIV-DRIVES/accessories
-HIVDIR="${pref_conda}/envs/HIV-DRIVES"
-mkdir "${HIVDIR}"/accessories
-mv algorithm "${HIVDIR}"/accessories/ && mv bin "${HIVDIR}"/accessories/ && mv data* "${HIVDIR}"/accessories/
+mv bin "${HIVDIR}"/accessories/ && mv data* "${HIVDIR}"/accessories/
 echo "export PATH=${HIVDIR}/accessories/bin/:$PATH" >>~/.bashrc
 source ~/.bashrc
 touch ../clean.sh &
